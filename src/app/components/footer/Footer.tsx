b7b16996bdf7df3aa9081151cc8e9977db3bde45
Kim Jae Hyeok <master_catch@naver.com>
2026-06-12 18:02:01 +0900

    feat : 푸터 공통 컴포넌트 추가

---
+++ src/app/components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from './style.module.css';
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>© 2026 YJ.SOFT. All rights reserved.</p>
+    </footer>
+  );
+}
